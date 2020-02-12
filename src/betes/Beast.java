23d0f7e789d49fed55b33883dbe8698d1f2512cd
avish <avish@DESKTOP-7943OT6>
2020-02-12 18:06:13 +0100

    Nooooormalement Bête il est fini mais bon on verra si il y a des choses a ajouter .. Et Wedding il a changer vite mais sans plus

--- src/betes/Beast.java
+++ src/betes/Beast.java
@@ -3,10 +3,12 @@ package betes;
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Random;
-
+import java.util.Scanner;
+import java.util.*;
 import java.awt.Color;
 
 public class Beast {
+	private String name;
 	private int age;
 	private String gender;
 	private Color color;
@@ -14,10 +16,11 @@ public class Beast {
 	private EnergyBar energybar;
 	private Characteristic charater;
 	private HashMap <Integer,String> hm;
-	private HashMap <Integer,Color> cl;
-	private ArrayList<Integer> al;
+	private ArrayList <String> nm;
+
 	
 	public Beast() {
+		this.name = name;
 		this.age = 0;
 		this.gender = randomGender();
 		this.color = getColor();
@@ -25,6 +28,17 @@ public class Beast {
 		this.charater = new Characteristic();
 		this.choice = new Antenna();
 	}
+	
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	
 	
 	public int getAge() {
 		return age;
@@ -35,12 +49,6 @@ public class Beast {
 		return age;
 	}
 	
-	public int Random() {
-		Random rm = new Random();
-		int n;
-		n = rm.nextInt(2);
-		return n;
-	}
 	
 	public void initGender() {
 		hm = new HashMap<Integer, String> ();
@@ -49,11 +57,40 @@ public class Beast {
 	} 
 	
 	public String randomGender() {
+		Random rm = new Random();
+		int n;
+		n = rm.nextInt(2);
 		initGender();
-		gender = hm.get(Random());
+		gender = hm.get(n);
 		return gender;
 	}
 	
+	
+	public int energy() {
+		return energybar.getEnergy();
+	}
+	
+	public String charact() {
+		int a;
+		int b;
+		int c;
+		int d;
+		int e;
+		
+		a = charater.getAgility();
+		b = charater.getIntelligence();
+		c = charater.getMadness();
+		d = charater.getStrength();
+		e = charater.getVelocity();
+		
+		String text = "";
+		text = "Agility = " + a + "\nVelocity = " + e + "\nMadness = " + c + "\nIntelligence = " + b + "\nStrength = " + d;
+		return text;
+	}
+	
+	public String ant() {
+		return choice.toString();
+	}
 	
 	public Color randomColor() {
 		Random rm = new Random();
@@ -68,20 +105,26 @@ public class Beast {
 		return z;
 	}
 	
-	
 	public Color getColor() {
 		color = randomColor();		
 		return color;
 	}
 	
+	public String toString() {
+		String text = "";
+		text += "\n\nName : " + getName() + "\nColor : " + getColor() + "\nAge : " + getAge() + "\nGender : " + randomGender() + "\nCharacteristic : \n" + charact() + "\n\nAntenna : \n" + ant() + "\n\nEnergy Bar : " + energy();
+		return text;
+	}
+	
 
 	public static void main (String[] args) {
 		Beast s = new Beast();
-		Characteristic ct = new Characteristic();
-		Antenna nt = new Antenna();
-		EnergyBar eb = new EnergyBar(20);
-		
-		System.out.println("Color : " + s.getColor() + "\nAge : " + s.getAge() + "\nGender : " + s.randomGender() + "\nCharacteristic : \n" + ct.toString() + "\n\nAntenna : \n" + nt.toString() + "\n\nEnergy Bar : " + eb.toString());
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Veuillez saisir un nom : ");
+		String name = sc.nextLine();
+		s.setName(name);
+		System.out.println(s.toString());
 		
 	}
 }
--- src/betes/Wedding.java
+++ src/betes/Wedding.java
@@ -6,20 +6,12 @@ public class Wedding {
 
 	private Beast scorpio1;
 	private Beast scorpio2;
-	private HashMap <String,String> hm;
+	private final HashMap <String,String> hm;
 	
 	public Wedding(Beast scorpio1, Beast scorpio2) {
-		hm = new HashMap<String, String> ();
+		hm = new HashMap<String,String> ();
 		this.scorpio1 = scorpio1;
 		this.scorpio2 = scorpio2;
-	}
-	
-	public void mariage(Beast scorpio1, Beast scorpio2) {
-		if(!alreadymaried(scorpio1, scorpio2)) {
-			if((scorpio1.randomGender().equals("male") && scorpio2.randomGender().equals("female")) || (scorpio1.randomGender().equals("female") && scorpio2.randomGender().equals("male"))) {
-				hm.put(scorpio1.getColor(), scorpio2.getColor());
-			}
-		}
 	}
 	
 	public boolean alreadymaried(Beast scorpio1, Beast scorpio2) {
@@ -27,7 +19,7 @@ public class Wedding {
 			return false;
 		}
 		else {
-			if(hm.containsKey(scorpio1.getColor()) || hm.containsKey(scorpio2.getColor())) {
+			if(hm.containsKey(scorpio1.getName()) || hm.containsKey(scorpio2.getName())) {
 				return true;
 			}
 			
@@ -36,6 +28,17 @@ public class Wedding {
 			}
 		}
 	}
+	
+	
+	public void mariage(Beast scorpio1, Beast scorpio2) {
+		if(!alreadymaried(scorpio1, scorpio2)) {
+			if((scorpio1.randomGender().equals("male") && scorpio2.randomGender().equals("female")) || (scorpio1.randomGender().equals("female") && scorpio2.randomGender().equals("male"))) {
+				hm.put(scorpio1.getName(), scorpio2.getName());
+			}
+		}
+	}
+	
+	
 	
 	
 }
