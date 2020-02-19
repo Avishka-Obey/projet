f265ec831c05b55a6ca3eb2593f3be046d7cad99
Enzo <Enzo@10.77.37.186>
2020-02-19 15:10:01 +0100

    avishka regarde

--- src/betes/Reproduction.java
+++ src/betes/Reproduction.java
@@ -6,16 +6,50 @@ public class Reproduction {
 	private Beast scorpio2;
 	private Beast scorpio3;
 	private Wedding w;
+	private Reproduction r;
 	
 	public Reproduction(Beast scorpio1, Beast scorpio2) {
 		this.scorpio1 = scorpio1;
 		this.scorpio2 = scorpio2;
-		this.w = new Wedding();
+		this.r= new Reproduction(scorpio1, scorpio2);
 	}
 	
-	public void reproduce() {
+	public Beast reproduce() {
 		if(w.mariage(scorpio1, scorpio2)) {
 			Beast scorpio3 = new Beast();
+			
 		}
+		return scorpio3;
+	}
+	public String toString() {
+		String texte="";
+		texte="scorpio1:"+ scorpio1.getName() + "scorpio2:"+ scorpio2.getName() + "donne naissance au Scorpio3"+scorpio3.getName();
+		return texte;
+	}
+
+	
+
+	public static void main(String[]args) {
+		Beast scorpio1 = new Beast();
+		Beast scorpio2= new Beast();
+		Beast scorpio3 = new Beast();
+		Beast.initName();
+		 scorpio1.randomName();
+		 scorpio2.randomName();
+		 scorpio3.randomName();
+		 Wedding w = new Wedding();
+		 if(w.mariage(scorpio1, scorpio2)) {
+			 Reproduction r = new Reproduction(scorpio1, scorpio2) ;
+			 r.toString() ;
+			 
+		 }
+		 else if((w.mariage(scorpio2, scorpio1))){
+			 Reproduction r = new Reproduction(scorpio2, scorpio1) ;
+			 r.toString() ;
+			 
+		 }
+		 else {
+			 System.out.println("Impossible pour " + scorpio1.getName() + " et " + scorpio2.getName() + " de se marier donc pas de mariage") ;
+		 }
 	}
 }
