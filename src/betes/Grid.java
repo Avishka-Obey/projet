b0d8e3866acb22306e37375c20c5e0db76560eda
RayaneDendoune <rayane.dendoune@gmail.com>
2020-02-20 19:21:28 +0100

    debut class grid

---
+++ src/betes/Grid.java
@@ -0,0 +1,23 @@
+package betes;
+
+import java.util.HashMap;
+
+public class Grid {
+	
+	private HashMap<Position,Square> ca;
+	private int nbCaseH;
+	private int nbCaseV;
+	
+	public Grid() {
+		int i;
+		int j ;
+		for(i=0 ; i<nbCaseH ; i++) {
+			for(j=0 ; j<nbCaseV ; j++) {
+				ca.put(Square.pos(),);
+			}
+		}
+		
+		
+	}
+
+}
