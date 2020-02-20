a1d23d5cb328d08dcf73132bb6bf14781f689c99
avish <avish@10.77.81.213>
2020-02-20 15:18:40 +0100

    Je sais pas quoi dire ..

---
+++ src/betes/Position.java
@@ -0,0 +1,34 @@
+package betes;
+
+public class Position {
+	private int x;
+	private int y;
+	
+	public Position(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public String toString() {
+		String text = "";
+		text += "L'abscisse est : " + getX() + "\nEt l'ordonnée est : " + getY();
+		return text;
+	}
+
+}
--- src/betes/Square.java
+++ src/betes/Square.java
@@ -1,21 +1,27 @@
 package betes;
 
 public class Square {
-	private int abs;
-	private int ord;
+	private Position pos;
+	private Environment env;
+	private Element e1;
+	private Element e2;
+	private int i;
+	private int j;
 	
-	public Square(int abs, int ord) {
-		this.abs = abs;
-		this.ord = ord;
+	public Square() {
+		this.pos = new Position(i, j);
+		this.env = new Environment();
+		this.e1 = null;
+		this.e2 = null;
 	}
 	
-	public int getAbs() {
-		return abs;
-	}
-	
-	public int getOrd() {
-		return ord;
-	}
+//	public int getAbs() {
+//		return abs;
+//	}
+//	
+//	public int getOrd() {
+//		return ord;
+//	}
 	
 	public static void main(String[] args) {
 		
