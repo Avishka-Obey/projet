f290e61cfaa780f21c6e5a0956f974991ca74f47
RayaneDendoune <rayane.dendoune@gmail.com>
2020-02-25 13:43:28 +0100

    interface leger

--- src/betes/Interface.java
+++ src/betes/Interface.java
@@ -46,31 +46,31 @@ public class Interface extends JFrame{
 		pan.add(panCenter, BorderLayout.CENTER);
 		panCenter.setBackground(Color.red) ;
 		
-		panNord.setMinimumSize(new Dimension(100,50));
-		panNord.setPreferredSize(new Dimension(100,50));
-		panNord.setMaximumSize(new Dimension(100,50));
+		panNord.setMinimumSize(new Dimension(0,0));
+		panNord.setPreferredSize(new Dimension(0,0));
+		panNord.setMaximumSize(new Dimension(0,0));
 		pan.add(panNord, BorderLayout.NORTH);
-		panNord.setBackground(Color.white);
+		panNord.setBackground(Color.gray);
 		
-		panSud.setMinimumSize(new Dimension(100,50));
-		panSud.setPreferredSize(new Dimension(100,50));
-		panSud.setMaximumSize(new Dimension(100,50));
+		panSud.setMinimumSize(new Dimension(200,100));
+		panSud.setPreferredSize(new Dimension(200,100));
+		panSud.setMaximumSize(new Dimension(200,100));
 		pan.add(panSud, BorderLayout.SOUTH);
-		panSud.setBackground(Color.white);
+		panSud.setBackground(Color.gray);
 		panSud.add(tx);
 		
 		panEst.setMinimumSize(new Dimension(100,50));
 		panEst.setPreferredSize(new Dimension(100,50));
 		panEst.setMaximumSize(new Dimension(100,50));
 		pan.add(panEst, BorderLayout.EAST);
-		panEst.setBackground(Color.white);
+		panEst.setBackground(Color.gray);
 		panEst.add(tr);
 		
 		panOuest.setMinimumSize(new Dimension(100,50));
 		panOuest.setPreferredSize(new Dimension(100,50));
 		panOuest.setMaximumSize(new Dimension(100,50));
 		pan.add(panOuest, BorderLayout.WEST);
-		panOuest.setBackground(Color.white);
+		panOuest.setBackground(Color.gray);
 		panOuest.add(env);
 		
 		    
