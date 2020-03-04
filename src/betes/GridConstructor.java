5fec891396066d3cd92836c36f35f42708c17bb4
RayaneDendoune <rayane.dendoune@gmail.com>
2020-03-04 14:28:02 +0100

    debut  image ca arrive lourd

--- src/betes/GridConstructor.java
+++ src/betes/GridConstructor.java
@@ -3,8 +3,14 @@ package betes;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
+
 
 
 public class GridConstructor extends JPanel {
@@ -20,6 +26,8 @@ public class GridConstructor extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		drawDebugGrid(g);
+		Graphics2D g2 = (Graphics2D) g;
+		printBeast(g2);
 	}
 	
 	private void drawDebugGrid(Graphics g) {
@@ -39,6 +47,21 @@ public class GridConstructor extends JPanel {
 		for (int i = 0; i <= height; i += 50) {
 			g.drawLine(1, i, width, i);
 		}
+	}
+	
+	
+	public static Image readImage(String filePath) {
+		try {
+			return ImageIO.read(new File(filePath));
+		} catch (IOException e) {
+			System.err.println("-- Can not read the image file ! --");
+			return null;
+		}
+	}
+	
+	private void printBeast(Graphics g2) {
+		g2.drawImage(readImage("src/images/scorpion.jpg"),50, 100, null, null);
+		
 	}
 	
 }
